22a1d6e7b5de9029fd409bdd57ce0af2c3fdd2bd
Jason Garber <jason@sixtwothree.org>
2020-12-10 15:13:48 -0500

    Use flatMap()

--- express-api/src/config/database.mjs
+++ express-api/src/config/database.mjs
@@ -2,4 +2,4 @@ import { readFileSync } from 'fs';
 
 export const photos = JSON.parse(readFileSync('../data/photos.json'));
 
-export const categories = Array.from(new Set(photos.map(photo => photo.category).flat().filter(Boolean).sort()));
+export const categories = Array.from(new Set(photos.flatMap(photo => photo.category).filter(Boolean).sort()));
